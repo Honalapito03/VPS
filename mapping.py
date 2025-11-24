b30fde8217995e21b1e7f5941807b3675bcc3afa
Honalapito03 <jatekgeri15@gmail.com>
2025-11-24 11:37:10 +0100

    Exporting measurement to excel

--- mapping.py
+++ mapping.py
@@ -1,9 +1,7 @@
 #To do:
 #1) Probabilistic coordinates
-#2)Reference picture finding
-#3) Rotation bug on l.27
-#?)
-#?) FMT, image taking
+#2) Rotation precision error handling
+#3) Scale diff max limit
 
 import torch
 import cv2
@@ -11,6 +9,8 @@ import torch_utils as tu
 import numpy as np
 import math
 import random
+import xlsxwriter
+
 from torch_CC_model import Network
 from torch_tranining import transform
 
@@ -42,6 +42,7 @@ class Coordinates():
         averaged_angle = math.atan2(average_sin, average_cos)
         return Coordinates((self.x+other.x)/2, (self.y+other.y)/2,(self.s+other.s)/2,(averaged_angle/math.pi)*180) #make shorter
     
+
     def create_affine_matrix(self):
         self.affine_matrix = np.array(
             [
@@ -70,13 +71,30 @@ class map_tile():
 class Mapper():
     def __init__(self, ):
         self.resolution = 100
+        self.x_res = 1
+        self.y_res = 1
         self.px_mm = 1
         self.tiles = []
         self.pos = None
         self.last_ref = None
         self.current_image = 0
         self.device = "cpu"
-        self.N = Network(argmax_beta=300.0, device=self.device).to(self.device)
+        self.N = Network(argmax_beta=1000.0, device=self.device).to(self.device)
+        self.take_history = False
+
+        self.h_x = []
+        self.h_y = []
+        self.h_s = []
+        self.h_r = []
+        self.h_last_ref = []
+        self.h_last_ref_cov = []
+        self.h_ref_count = []
+        self.h_ref_avr_cov = []
+
+        self.gt_x = []
+        self.gt_y = []
+        self.gt_s = []
+        self.gt_r = []
 
 
 
@@ -117,6 +135,23 @@ class Mapper():
         print(len(self.tiles))
         print("----")
 
+        #History
+        if self.take_history:
+            self.h_x.append(self.pos.x * self.px_mm)
+            self.h_y.append(self.pos.y * self.px_mm)
+            self.h_s.append(self.pos.s)
+            self.h_r.append(self.pos.r)
+            self.h_last_ref.append(self.tiles.index(self.last_ref))
+            self.h_last_ref_cov.append(self.last_ref.coverage_test(self.pos, self.resolution))
+            self.h_ref_count.append(len(refs))
+            covs = [tile.coverage_test(self.pos, self.resolution) for tile in refs] 
+            if len(covs) > 0:
+                self.h_ref_avr_cov.append(sum(covs)/len(covs))
+            else:
+                self.h_ref_avr_cov.append(0)
+
+
+
     def ref_pic_find(self, coordinates:Coordinates, tiles:list[map_tile]) -> list[map_tile]:
         good_tiles = []
         for tile in tiles:
@@ -127,12 +162,12 @@ class Mapper():
 
 
     def FMT(self, picture:np.ndarray, template:np.ndarray) -> Coordinates:
-        img_o = torch.tensor(picture, dtype=torch.float32, device=self.device).transpose(0, 2).transpose(1, 2).unsqueeze(0)[:, :, 2:-2, 2:-2]
-        temp_o = torch.tensor(template, dtype=torch.float32, device=self.device).transpose(0, 2).transpose(1, 2).unsqueeze(0)[:, :, 2:-2, 2:-2]
+        img_o = torch.tensor(picture, dtype=torch.float32, device=self.device).unsqueeze(2).transpose(0, 2).transpose(1, 2).unsqueeze(0)[:, :, 2:-2, 2:-2]
+        temp_o = torch.tensor(template, dtype=torch.float32, device=self.device).unsqueeze(2).transpose(0, 2).transpose(1, 2).unsqueeze(0)[:, :, 2:-2, 2:-2]
         res, _, _ = self.N(img_o, temp_o, False)
         print("FMT result:", res)
 
-        return Coordinates(res[0][2].item() * self.resolution,res[0][3].item() * self.resolution,res[0][1].item(),res[0][0].item())
+        return Coordinates(res[0][2].item() * self.x_res, res[0][3].item() * self.y_res, res[0][1].item(), res[0][0].item())
 
 
     def rec_merge(self, coordinates:list) -> Coordinates:
@@ -144,6 +179,37 @@ class Mapper():
             return coordinates[0].merge(coordinates[1])
         else:
             return coordinates[0]
+        
+    def add_gt(self, x, y, s, r):
+        self.gt_x.append(x)
+        self.gt_y.append(y)
+        self.gt_s.append(s)
+        self.gt_r.append(r)
+    
+    def export_history(self, filename):
+        workbook = xlsxwriter.Workbook(filename)
+        worksheet = workbook.add_worksheet()
+
+        headers = ['est_x_mm', 'est_y_mm', 'est_s', 'est_r', 'last_ref_index', 'last_ref_cov', 'ref_count', 'ref_avr_cov', 'gt_x_mm', 'gt_y_mm',  'gt_s', 'gt_r']
+        for col_num, header in enumerate(headers):
+            worksheet.write(0, col_num, header)
+
+        for row in range(len(self.h_x)):
+            worksheet.write(row + 1, 0, self.h_x[row])
+            worksheet.write(row + 1, 1, self.h_y[row])
+            worksheet.write(row + 1, 2, self.h_s[row])
+            worksheet.write(row + 1, 3, self.h_r[row])
+            worksheet.write(row + 1, 4, self.h_last_ref[row])
+            worksheet.write(row + 1, 5, self.h_last_ref_cov[row])
+            worksheet.write(row + 1, 6, self.h_ref_count[row])
+            worksheet.write(row + 1, 7, self.h_ref_avr_cov[row])
+            if row < len(self.gt_x):
+                worksheet.write(row + 1, 8, self.gt_x[row])
+                worksheet.write(row + 1, 9, self.gt_y[row])
+                worksheet.write(row + 1, 10, self.gt_s[row])
+                worksheet.write(row + 1, 11, self.gt_r[row])
+
+        workbook.close()
 
 
 if __name__ == "__main__":
